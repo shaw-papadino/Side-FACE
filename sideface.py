cb4dbb076a0cc24eb9741839f9d19647c97e4c48
Shbot <Shbot@github.io>
2018-06-23 09:34:38 +0900

    second
    second

--- sideface.py
+++ sideface.py
@@ -5,25 +5,21 @@ import glob
 import collections
 import os
 import shutil
-import threading
-import logging
 import time
+import random
 
-global_lock = threading.Lock()
-logging.basicConfig(level=logging.DEBUG,
-                    format='[%(levelname)s] (%(threadName)-10s) %(message)s',
-                    )
 
 path = "/Users/okayamashoya/NeoTrainingAssistant/static/img_dst/"
 #テストデータ "20180424113542/", 学習データ "201804280930/"
-path_test = path + "20180424113542/"
-
-#1層目の検出器
+path_test = path + "201804280930/"
+#path_test = path + "20180424113542/"
+#/Users/okayamashoya/Pictures/20180602/side
+#1層目の検出器 "201804280930/"
 path_1 = path + "201804280930/"
 #2層目の検出器
 path_2 = path + "201805071842/"
 #カスケード
-cascade_path = 'cascade/lbp/cascade.xml'
+cascade_path = 'cascade/lbp_in_out_aki_tsuji/cascade.xml'
 #画像入れ込み
 path_dir = path + "img_croppy/"
 #パラメータ
@@ -32,8 +28,7 @@ P_P = 0
 P_N = 0
 N_N = 0
 N_P = 0
-Dis_P = 0
-Dis_N = 0
+Dis = 0
 noise = 0
 
 #検出器読み込み
@@ -41,123 +36,164 @@ sideface_cascade = cv2.CascadeClassifier(path_1 + cascade_path)
 second_cascade = cv2.CascadeClassifier(path_2 + cascade_path)
 
 #Positive数読み込み
-P_s= sum(1 for line in open(path_test + 'positive.dat'))
+P_s= sum(1 for line in open(path_test + 'positive_in_out_aki_tsuji.dat'))
 #Negative数読み込み
-N_s = sum(1 for line in open(path_test + 'negative.dat'))
+N_s = sum(1 for line in open(path_test + 'negative_in_out_aki_tsuji.dat'))
 #全画像読み込み
 file_lists = glob.glob(path_test + "img/*.jpg")
+random.seed(0)
+file_lists_r = random.sample(file_lists, 1000)
 
 #Positiveリスト読み込み
-P_file = open(path_test + 'positive.dat', 'r')
+P_file = open(path_test + 'positive_in_out_aki_tsuji.dat', 'r')
 #Negativeリスト読み込み
-N_file = open(path_test + 'negative.dat', "r")
+N_file = open(path_test + 'negative_in_out_aki_tsuji.dat', "r")
 
 #1行ずつリストにする
 P_f = P_file.readlines()
 N_f = N_file.readlines()
 
-noise_dir = "noise"
-P_P_dir = "P_P"
-P_N_dir = "P_N"
-N_P_dir = "N_P"
+noise_dir = "noise0612"
+
+P_N_dir = "P_N_0623/"
+P_P_dir = "P_P_0623/"
+N_P_dir = "N_P_0623/"
+for dir0 in [P_P_dir, N_P_dir]:
+    filepath = path_dir + dir0
+    if not os.path.exists(filepath):
+            os.mkdir(filepath)
+
+
+
+
 def remove(filepath):
     files = os.listdir(filepath)
     if len(files) > 0 :
         os.remove(filepath + "/*.jpg")
-        
+    
 def write(filepath):
     if not os.path.exists(filepath):
         os.mkdir(filepath)
     cv2.imwrite(filepath + f_remove , img)
+def write1(filepath):
+    if not os.path.exists(filepath):
+        os.mkdir(filepath)
+    cv2.imwrite(filepath + f_remove , img1)
 
+def write2(filepath):
+    if not os.path.exists(filepath):
+        os.mkdir(filepath)
+    cv2.imwrite(filepath + f_remove , img2)
+
+def write3(filepath):
+    if not os.path.exists(filepath):
+        os.mkdir(filepath)
+    cv2.imwrite(filepath + f_remove , img3)
+    
 def sideface_core1():
-    global global_lock
-    logging.debug('Starting')
+    
     for file_list in file_lists:
         global img
         img = cv2.imread(file_list, cv2.IMREAD_COLOR)
+        global yAxis
+        yAxis = cv2.flip(img, 1)
         # グレースケール変換
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(yAxis, cv2.COLOR_BGR2GRAY)
+        
 
         #横顔を検知
         faces = sideface_cascade.detectMultiScale(gray, 1.1, 3)
 
         #消す
-        global f_remove
-        f_remove = file_list.replace(path_test + 'img', '')
-
-        global_lock.acquire()
-        if len(faces) > 0:
-            #ポジティブリストに入っているか
-            l_in = [s for s in P_f if f_remove in s]
-            #画像切り取り
-            img = img[faces[0][1]:faces[0][1]+faces[0][3], faces[0][0]:faces[0][0]+faces[0][2]]
-
+        #global f_remove
+        #f_remove = file_list.replace(path_test + "img/", '')
+        f_name = os.path.splitext(os.path.basename(file_list))[0]
             
-            if len(l_in) != 0:
-                #横顔と認識していないものを切り取る
-                if img.shape[0] < 400:
-                    global noise
-                    noise += 1
-                    write(path_dir + noise_dir)
-                    global_lock.release()
+##          #ポジティブリストに入っているか
+        l_in = [s for s in P_f if f_name in s]
+        #画像切り取り
+        global img1
 
+        
+        if len(l_in) != 0:
+            #横顔と認識していないものを切り取る
+##                if img.shape[0] < 400:
+##                    global noise
+##                    noise += 1
+##                    write(path_dir + noise_dir)
+            global P_P
+
+            fa_p = 0
+            fa_n = 0
+            for (x, y, w, h) in faces:
+                img1 = img[y:y + h,x: x + w]
+                if w * h > 400 * 400:
+                    
+                    cv2.imwrite(path_dir + P_P_dir + f_name + str(fa_p) + "L" + ".jpg", img1)
+                    fa_p += 1
                 else:
-                    global P_P
-                    P_P += 1
-                    write(path_dir + P_P_dir)
-                    global_lock.release()
+                    cv2.imwrite(path_dir + P_P_dir + f_name + str(fa_n) + "l" +  ".jpg", img1)
+                    fa_n += 1
+                    
 
+            if fa_p > 0 :
+                P_P += 1
             else:
-                l_in = [s for s in N_f if f_remove in s]
-                if len(l_in) != 0:
-                    # グレースケール変換
-                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    #笑っていない横顔を検出
-                    second = second_cascade.detectMultiScale(gray, 1.1, 3)
-
-                    #横顔と認識していないものを切り取る
-                    if img.shape[0] < 400:
-                        noise += 1
-                        write(path_dir + noise_dir)
-                        global_lock.release()
-
-                    elif len(second) > 0:
-                        global N_N
-                        N_N += 1
-                        global_lock.release()
-                    else:
-                        global N_P
-                        N_P += 1
-                        write(path_dir + N_P_dir)
-                        global_lock.release()
-
-                else:
-                    global Dis_P
-                    Dis_P += 1
-                    global_lock.release()
-
-            for rect in faces:
-                cv2.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]),(0, 0, 255), thickness=2)
-
+                global P_N
+                P_N += 1
+                     
+                
+                    
 
         else:
-            l_in = [s for s in N_f if f_remove in s]
+            l_in = [s for s in N_f if f_name in s]
             if len(l_in) != 0:
-                N_N += 1
-                global_lock.release()
-            else:
-                l_in = [s for s in P_f if f_remove in s]
-                if len(l_in) != 0:
-                    global P_N
-                    P_N += 1
-                    write(path_dir + P_N_dir)
-                    global_lock.release()
+                global N_P
+                fa_p2 = 0
+                fa_n2 = 0
+                for (x, y, w,h ) in faces:
+                    img1 = img[y:y + h,x: x + w]
+                    if w * h > 400 * 400:
 
+                        cv2.imwrite(path_dir + N_P_dir + f_name + str(fa_p2) + "L" + ".jpg", img1)
+                        fa_p2 += 1
+                    else:
+                        cv2.imwrite(path_dir + N_P_dir + f_name + str(fa_n2) + "l" + ".jpg", img1)
+                        fa_n2 += 1
+                        
+
+                if fa_p2 > 0 :
+                    N_P += 1
                 else:
-                    global Dis_N
-                    Dis_N += 1
-                    global_lock.release()
+                    global N_N
+                    N_N += 1
+                
+                    
+
+            else:
+                global Dis
+                Dis += 1
+                    
+##
+##            for rect in faces:
+##                cv2.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]),(0, 0, 255), thickness=2)
+
+
+    
+##    else:
+##        print("o")
+##        minmum_cascade = cv2.CascadeClassifier(path_2 + 'cascade/lbp/cascade.xml')
+##        file_lists_p = glob.glob(path + "img_croppy/"+ P_P_dir +"/*.jpg")
+##        for file_list_p in file_lists_p:
+##            img_p = cv2.imread(file_list_p, cv2.IMREAD_COLOR)
+##            grayp = cv2.cvtColor(img_p, cv2.COLOR_BGR2GRAY)
+##            minmum = minmum_cascade.detectMultiScale(grayp, 1.1, 3)
+##            f_remove = file_list_p.replace(path_dir + P_P_dir , '')
+##            if len(minmum) > 0:
+##                P_P -= 1
+##                global noise
+##                noise += 1
+##                shutil.move(path_dir + P_P_dir + f_remove, path_dir + noise_dir + "/")
 
 ##else:
 ##    minmum_cascade = cv2.CascadeClassifier(path_min + 'cascade/lbp/cascade.xml')
@@ -187,12 +223,7 @@ def sideface_core1():
 ##                shutil.move(path_min + "img_croppy/P_P/" + f_remove, path_min + "img_croppy/noise/")
 
 if __name__ == "__main__":
-    thread_1 = threading.Thread(target=sideface_core1)
-    
-
-    thread_1.start()
-    
-    thread_1.join()
+    sideface_core1()
     
     print("All : {0} ".format(len(file_lists)))
     print("P_s : {0} ".format(P_s))
@@ -201,9 +232,8 @@ if __name__ == "__main__":
     print("P_N : {0}".format(P_N))
     print("N_N : {0}".format(N_N))
     print("N_P : {0}".format(N_P))
-    print("noise : {0}".format(noise))
-    print("Dis_P : {0}".format(Dis_P))
-    print("Dis_N : {0}".format(Dis_N))
+    #print("noise : {0}".format(noise))
+    print("Dis: {0}".format(Dis))
 
 
 # 画像表示
